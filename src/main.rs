09983af3a44717fb02664c3b71c48509ade6f9f9
Radek Krahl <radek@krahl.pl>
2022-12-17 14:53:41 +0100

    4.5.10 -> tracing enabled, moved to telemetry.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
