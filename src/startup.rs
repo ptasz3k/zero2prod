4b6e9b05767fe35b0c670e6891ccad9f2fd36d0f
Radek Krahl <radek@krahl.pl>
2022-12-17 18:41:39 +0100

    Finished chapter 4 - using actix-web tracing middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
-use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
@@ -10,7 +10,7 @@ pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error
     let connection = web::Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
