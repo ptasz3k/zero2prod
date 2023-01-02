ebdd4953d4923d8f298643f69f72d7ece61c6250
Radek Krahl <radek@krahl.pl>
2023-01-02 22:20:02 +0100

    7.6.5 - database migration done

---
+++ migrations/20230102210503_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+begin;
+  update subscriptions
+  set status = 'confirmed'
+  where status is null;
+
+  alter table subscriptions alter column status set not null;
+
+commit;
