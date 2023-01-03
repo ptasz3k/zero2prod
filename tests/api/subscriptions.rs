9486ed5f8746fa14db2c2985ff73a98763c0afb7
Radek Krahl <radek@krahl.pl>
2023-01-03 22:32:43 +0100

    7.7.3

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -4,7 +4,7 @@ use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let test_app = spawn_app().await;
+    let app = spawn_app().await;
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -12,19 +12,33 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .expect(1)
-        .mount(&test_app.email_server)
+        .mount(&app.email_server)
         .await;
 
-    let response = test_app.post_subscriptions(body.into()).await;
+    let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("select email, name from subscriptions",)
-        .fetch_one(&test_app.db_pool)
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("select email, name, status from subscriptions",)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
